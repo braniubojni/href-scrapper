a09751cce2a24b3a9ad6ea769c64d079827be37b
Erik <braniubojni@mail.ru>
2022-02-05 16:25:20 +0400

    need to be reviewed

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { IWebsite } from "./interfaces";
+
+const getSubLinkStatus = async (urls: string[]) => {
+  const promiseUrls: any = [];
+
+  urls.forEach((url) => {
+    promiseUrls.push(axios.get(url));
+  });
+  return await Promise.all(promiseUrls);
+};
+
+const extractLink = async (url: string) => {
+  try {
+    const pattern = new RegExp('href="' + url + "[^#][a-zA-Z0-9-]+", "g");
+    const scanned = new Set();
+
+    // Get homepage
+    const { data } = await axios.get(url);
+
+    const setQueue = [
+      ...new Set(
+        data.match(pattern).map((url: string) => url.replace('href="', ""))
+      ),
+    ];
+    (await getSubLinkStatus(setQueue as string[])).forEach((link) => {
+      scanned.add({
+        _website: [url],
+        _link: [link.request.res.responseUrl],
+        statusCode: [link.status],
+      });
+    });
+    return scanned;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const main = async (urls: IWebsite[]) => {
+  if (urls.length <= 0) {
+    return 0;
+  }
+  return await Promise.all(
+    urls.map((url) => {
+      return extractLink(url._website[0]);
+    })
+  );
+};
+
+main([
+  { _website: ["https://hexact.io/"] },
+  { _website: ["https://stackoverflow.com/"] },
+])
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
